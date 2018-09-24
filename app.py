bfcf07182e59e690274609edce76a4cd42d460cd
philippG777 <philippG777@protonmail.com>
2018-09-24 21:02:21 +0200

    Add app-object

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
+app = Flask(__name__)
+
 @app.route("/")
 def index():
     return render_template("index.html")
