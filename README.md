211ee033941b948dc83e97aa4c055da9e8381642
philippG777 <philippG777@protonmail.com>
2018-09-24 20:59:44 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wuzzel-ranking
 This repository contains a simple ranking-system for wuzzeln.
+
+**NOTE: This project is still in development!**
---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/auth")
+def auth():
+    return "auth-page"
+
+
+if __name__ == "__main__":
+    app.run()
