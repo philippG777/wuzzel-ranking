aab0f230278f5448f70af41ca832141a001743d6
philippG777 <philippG777@protonmail.com>
2018-10-09 20:15:08 +0200

    Rewrite User class

--- app.py
+++ app.py
@@ -1,77 +1,92 @@
 from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, login_required
+from flask_login import current_user, login_required, login_user, LoginManager,
+                        logout_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
 import config
 
 
-# for testing
-def generate_test_user():
-    return User(0, "test", generate_password_hash("test"))
+SQLALCHEMY_DATABASE_URI = ("mysql+mysqlconnector://{username}:"
+                           "{password}@{hostname}/{databasename}").format(
+    username = config.db_user,
+    password = config.db_password,
+    hostname = config.db_hostname,
+    databasename = config.db_name
+)
+
+app = Flask(__name__)
+app.config["DEBUG"] = False
+app.config["SECRET_KEY"] = config.secret_key
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-def query_user_by_id(id):
-    if id == 0:
-        return generate_test_user()
-    return None
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
 
-
-def query_user_by_name(name):
-    if name == "test":
-        return generate_test_user()
-    return None
-
-
-class User(object):
-    @staticmethod
-    def get(user_id):
-        return query_user_by_id(user_id)
-    
-    @staticmethod
-    def get_by_name(name):
-        return query_user_by_name(name)
-
-    def __init__(self, id, name, password_hash):
-        self.id = id
-        self.name = name
-        self.hash = password_hash
-
-        self.is_authenticated = False
-        self.is_active = True
-        self.is_anonymus = False
-    
-    def get_id(self):
-        return u"%d" % self.id
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
+    wins = db.Column(db.Integer)
+    losses = db.Column(db.Integer)
 
     def check_password(self, password):
-        return check_password_hash(self.hash, password)
+        return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        return self.username
 
 
-login_manager = LoginManager()
-app = Flask(__name__)
-login_manager.init_app(app)
+class Game(db.Model):
+    __tablename__ = "games"
+
+    id = db.Column(db.Integer, primary_key=True)
+    played = db.Column(db.DateTime, default=datetime.now)
+    winner_front_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    winner_back_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    looser_front_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    looser_back_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    
+    winner_front = db.relationship('User', foreign_keys=winner_front_id)
+    winner_back = db.relationship('User', foreign_keys=winner_back_id)
+    looser_front = db.relationship('User', foreign_keys=looser_front_id)
+    looser_back = db.relationship('User', foreign_keys=looser_back_id)
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    return User.query.filter_by(username=user_id).first()
 
 @app.route("/")
 def index():
+    if current_user.is_authenticated:
+        return redirect("/dashboard/")
     return render_template("login.html")
 
-@app.route("/login", methods=["POST"])
+@app.route("/login/", methods=["POST"])
 def login():
     username = request.form["u"]
     password = request.form["p"]
 
-    user = User.get_by_name(username)
+    user = load_user(username)
     if user is not None and user.check_password(password):
         login_user(user)
         return redirect("/dashboard")
-    return "Login failed"
+    return redirect("/")
 
-@app.route("/dashboard")
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+@app.route("/dashboard/")
 @login_required
 def dashboard():
     return render_template("dashboard.html")
